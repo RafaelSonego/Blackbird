68c02fa6a5cb9c868b6cb4481151f73cf9d36f8b
Rob Foulkrod <rob foulkrod>
2022-04-11 15:39:37 -0400

    UserProfile Initial Thoughts #4098

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using blackbird.web.Models;
+
+namespace blackbird.web.Controllers;
+
+public class UserProfileController : Controller
+{
+    private readonly ILogger<UserProfileController> _logger;
+
+    public HomeController(ILogger<UserProfileController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
